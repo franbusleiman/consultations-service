03b1abf782fa6acbe80146ed658503a3697d5927
franbusleiman <franbusleiman@gmail.com>
2024-06-26 15:00:37 -0300

    c

---
+++ src/main/java/com/liro/consultations/config/FlyWayConfig.java
@@ -0,0 +1,30 @@
+package com.liro.consultations.config;
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlyWayConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dataSourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dataSourcePassword;
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        DataSource dataSource = new DriverManagerDataSource(dataSourceUrl, dataSourceUsername, dataSourcePassword);
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .load();
+    }
+}
